351f1503b92b785c8349c429b84a159cc0cbad29
michaelsoft <michaellyk@qq.com>
2021-03-05 15:17:34 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketTest.Test();
+
+            Console.Read();
+        }
+
+    }
+}
